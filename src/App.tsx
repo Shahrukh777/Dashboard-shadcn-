e2bd40dbade2d3bc266e2d3f1857c90588b9ff34
Shahrukh777 <sheikhshahrukh904@gmail.com>
2025-09-05 20:30:36 +0530

    initial commit. shadcn config done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div>App
+      <Button className='hover:bg-primary/75 rounded-2xl' size="lg"> Save </Button>
+    </div>
+  )
+}
+
+export default App
